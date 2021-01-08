7c0fa0ac5f4347d8b5fa3616825590f5f146f816
Anubosiris <76901115+Develaupair@users.noreply.github.com>
2021-01-08 10:00:22 +0100

    Added Copyright Notice

--- st.bat
+++ st.bat
@@ -1,3 +1,4 @@
+::Copyright (C) 2021 Anubosiris
 @if "%1" EQU "" (set /a s=1200) else (set /a s=%1)
 @echo System goes to hibernation in %s% seconds. Press CTRL+C to cancel hibernation. Press any key to hibernate instantly.
 @timeout /t %s%
